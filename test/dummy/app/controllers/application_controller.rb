0f64016d4b21d7b7401e418f827c89846bbe16d4
etapeta <cpetasecca@gmail.com>
2011-07-05 08:36:06 +0200

    configure surveys in dummy

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def all_surveys
+    @all_surveys ||= Surveyor::Parser.parse_string File.read(Rails.root.join('config', 'all.survey'))
+  end
+
 end
