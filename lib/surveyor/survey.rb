70239bfcbaeacf96535a12d30d231d2b4f60169f
etapeta <cpetasecca@gmail.com>
2011-07-06 06:56:39 +0200

    introducing html_coder as expert for HTML output

--- lib/surveyor/survey.rb
+++ lib/surveyor/survey.rb
@@ -8,9 +8,10 @@ module Surveyor
     class HtmlCoder < Surveyor::Element::HtmlCoder
       include ActionView::Helpers::FormTagHelper
 
-      def generate(survey, object, options)
+      def emit_form(object, url, options)
         singular = ActiveModel::Naming.singular(object)
-        html_options = if object.respond_to?(:persisted?) && object.persisted?
+        html_options = {}
+        if object.respond_to?(:persisted?) && object.persisted?
           {
             :class  => "edit",
             :id => options[:id] || ActionController::RecordIdentifier.dom_id(object, :edit),
@@ -23,23 +24,46 @@ module Surveyor
             :method => :post
           }
         end
+        html_options['method'] = options[:method].to_s if options[:method]
+        html_options[:enctype] = "multipart/form-data" if options[:multipart]
+        html_options[:action]  = url
+        html_options['accept-charset'] = "UTF-8"
+        # html_options["data-remote"] = true if options["remote"]
 
-        dom_namer = DomNamer.new(survey.name, options[:id] || survey.name)
+        dom_namer = DomNamer.new(element.name, options[:id] || element.name)
         output = ActiveSupport::SafeBuffer.new
-        output.safe_concat(tag('div', {:class => 'survey', :id => dom_namer.id}, true))
         output.safe_concat(form_tag_html(html_options))
-        survey.elements.each do |elem|
-          elem.emit_html(output, object.send(elem.name), dom_namer + elem, elem.options)
-        end
+        emit(output, object, dom_namer, options)
+
+        # submit button
+        action = 'Submit'
+        output.safe_concat("<div class='buttons'><input type='submit' class='button' value='#{action}'/></div>")
         output.safe_concat("</form>")
-        output.safe_concat("</div>")
         output
       end
+
+      def emit(output, object, dom_namer, options)
+        output.safe_concat(tag('div', {:class => 'survey', :id => dom_namer.id}, true))
+        element.elements.each do |elem|
+          if Section === elem
+            elem.html_coder.emit(output, object, dom_namer, elem.options)
+          else
+            elem.html_coder.emit(output, object.send(elem.name), dom_namer + elem, elem.options)
+          end
+        end
+        output.safe_concat("</div>")
+      end
+
+    end
+
+    # create a html expert that represents object as an element in HTML.
+    def html_coder
+      HtmlCoder.new(self)
     end
 
     def form_for(object, form_options = {})
       # TODO: form_options can contain elements which should be hidden or readonly
-      HtmlCoder.new.generate(self, object, options)
+      html_coder.emit_form(object, form_options[:url], options.merge(form_options))
     end
 
   end
