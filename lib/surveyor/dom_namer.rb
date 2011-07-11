7710db47669b1bd85a826eb7f185d012eca09f35
etapeta <cpetasecca@gmail.com>
2011-07-11 03:23:09 +0200

    dom namer in module Surveyor

--- lib/surveyor/dom_namer.rb
+++ lib/surveyor/dom_namer.rb
@@ -1,29 +1,31 @@
-class DomNamer
-  attr_reader :name, :id
+module Surveyor
+  class DomNamer
+    attr_reader :name, :id
 
-  def self.start(element)
-    new(element.name, element.options[:id] || element.name)
+    def self.start(element)
+      new(element.name, element.options[:id] || element.name)
+    end
+
+    def initialize(name, id)
+      @name = name
+      @id = id
+    end
+
+    def plus(elem)
+      self.class.new("#{@name}[#{elem.name}]", "#{@id}:#{elem.options[:id] || elem.name}")
+    end
+
+    def +(elem)
+      plus(elem)
+    end
+
+    def mult(index)
+      self.class.new("#{@name}[]", "#{@id}:#{index}")
+    end
+
+    def *(index)
+      mult(index)
+    end
+
   end
-
-  def initialize(name, id)
-    @name = name
-    @id = id
-  end
-
-  def plus(elem)
-    self.class.new("#{@name}[#{elem.name}]", "#{@id}:#{elem.options[:id] || elem.name}")
-  end
-
-  def +(elem)
-    plus(elem)
-  end
-
-  def mult(index)
-    self.class.new("#{@name}[]", "#{@id}:#{index}")
-  end
-
-  def *(index)
-    mult(index)
-  end
-
 end
