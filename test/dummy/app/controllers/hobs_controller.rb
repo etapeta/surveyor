f74959ec37d9030158e0cc1493bb81680d703936
etapeta <cpetasecca@gmail.com>
2011-07-15 14:19:46 +0200

    include survey sheets in interfaces

--- test/dummy/app/controllers/hobs_controller.rb
+++ test/dummy/app/controllers/hobs_controller.rb
@@ -1,8 +1,7 @@
 class HobsController < ApplicationController
   before_filter :find_survey
 
-  # GET /nests
-  # GET /nests.xml
+  # GET /hobs
   def index
     nests = Nest.all.group_by(&:survey_name)
     @surveys = all_surveys.inject(Hash[]) {|hash,surv|
@@ -11,23 +10,20 @@ class HobsController < ApplicationController
     }
   end
 
-  # GET /nests/1
-  # GET /nests/1.xml
+  # GET /hobs/1
   def show
     @nest = Nest.find(params[:nest_id])
   end
 
-  # GET /nests/new
-  # GET /nests/new.xml
+  # GET /hobs/new
   def new
     @nest = nil
-    @hob = Surveyor::Hob.new(@survey)
+    @hob = Surveyor::Hob.new(@base_survey)
   end
 
-  # POST /nests
-  # POST /nests.xml
+  # POST /hobs
   def create
-    @hob = Surveyor::Hob.new(@survey)
+    @hob = Surveyor::Hob.new(@base_survey)
     @hob.update(params[@survey.name])
     nest = Nest.new(:survey_name => @survey.name)
     nest.document = @hob.to_h
@@ -35,31 +31,42 @@ class HobsController < ApplicationController
     redirect_to hobs_path
   end
 
-  # GET /nests/1/edit
+  # GET /hobs/1/edit
   def edit
     @nest = Nest.find(params[:id])
-    @hob = Surveyor::Hob.new(@survey, @nest.document)
+    @hob = Surveyor::Hob.new(@base_survey, @nest.document)
   end
 
-  # PUT /nests/1
-  # PUT /nests/1.xml
+  # PUT /hobs/1
   def update
     @nest = Nest.find(params[:id])
     @hob = Surveyor::Hob.new(@survey, @nest.document)
     @hob.update(params[@survey.name])
     if @hob.valid?
-      @nest.document = @hob.to_h
+      hob = Surveyor::Hob.new(@base_survey, @nest.document)
+      hob.update(params[@survey.name])
+      @nest.document = hob.to_h
       @nest.save!
       redirect_to hobs_path
     else
-      render :action => :edit
+      render :action => :edit, :id => params[:id], :survey => params[:survey], :sheet => params[:sheet]
     end
   end
 
   private
 
   def find_survey
-    @survey = params[:survey] && all_surveys.detect {|surv| surv.name == params[:survey] }
+    logger.info ">>> Session: #{session.inspect}"
+    if params[:survey]
+      @base_survey = all_surveys.detect {|surv| surv.name == params[:survey] }
+      @survey = if params[:sheet].blank?
+        session[:sheet] = nil
+        @base_survey
+      else
+        session[:sheet] = params[:sheet]
+        @base_survey.apply_sheet(params[:sheet])
+      end
+    end
   end
 
 end
